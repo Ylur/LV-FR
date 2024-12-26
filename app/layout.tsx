92ab60aa253de58fd2f3b2381b33e991feb920ea
Ingi Haralds <ingiharaldss@gmail.com>
2024-12-26 18:59:35 +0000

    fjarlægt nested

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { AuthProvider } from "./context/AuthContext";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AuthProvider>
+
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -39,6 +39,6 @@ export default function RootLayout({
           <Footer />
         </body>
       </html>
-    </AuthProvider>
+
   );
 }
